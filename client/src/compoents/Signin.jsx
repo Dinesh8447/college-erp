adea9efa3ba2bfda6ffd6715b8f8a1065f478d50
Dinesh8447 <219bca04dinesh@gmail.com>
2024-02-01 13:22:31 +0530

    server is created

--- client/src/compoents/Signin.jsx
+++ client/src/compoents/Signin.jsx
@@ -50,72 +50,74 @@ export default function Signin() {
   
     return (
       <div className='min-h-screen mt-20'>
-        <div className='flex p-3 max-w-3xl mx-auto gap-5 flex-col md:flex-row md:items-center'>
-          {/* left side */}
-          <div className='flex-1'>
-            <Link to={'/'} className=' text-4xl  font-bold dark:text-white'>
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mern</span>Blog
-            </Link>
-            <p className='text-sm mt-5 font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolor numquam recusandae ratione enim in ipsum explicabo eius corporis minus porro sint aperiam, eligendi illo tempora cumque voluptate. Iure, aut!</p>
-          </div>
-  
-  
-          {/* right side */}
-          <div className='flex-1'>
-            <form onSubmit={handlesubmit} className='flex flex-col gap-4'>
-  {/* email */}
-              <div>
-                <Label value='username' />
-                <TextInput
-                  type='text'
-                  placeholder='Username...'
-                  id='username'
-                  onChange={handlechange}
-                />
-              </div>
-  
-  {/* password */}
-              <div>
-                <Label value='Password' />
-                <TextInput
-                  type='password'
-                  placeholder='Password'
-                  id='password'
-                  onChange={handlechange}
-                />
-              </div>
-  
-  
-  
-  
-            <Button gradientDuoTone='purpleToPink' type='submit' >
-              {/* {
-                loading ? (
-                  <>
-                <Spinner size='sm'/>
-                <span className='pl-3'>Loading...</span> 
-                </>
-                )
-                :"Sign In"
-              } */}
-
-              signin
-            </Button>
-  {/* <Oauth/> */}
-  
-  
-            </form>
-  
-            {/* {
-              error && (
-                <Alert className='mt-5' color='failure'>
-                  {error}
-                </Alert>
-              )
-            } */}
-          </div>
+       <div className='flex p-3 max-w-3xl mx-auto gap-5 flex-col md:flex-row md:items-center'>
+        {/* left side */}
+        <div className='flex-1'>
+          <Link to={'/'} className=' text-4xl  font-bold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Mern</span>Blog
+          </Link>
+          <p className='text-sm mt-5 font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolor numquam recusandae ratione enim in ipsum explicabo eius corporis minus porro sint aperiam, eligendi illo tempora cumque voluptate. Iure, aut!</p>
         </div>
-  
+      
+      
+        {/* right side */}
+        <div className='flex-1'>
+          <form onSubmit={handlesubmit} className='flex flex-col gap-4'>
+      {/* email */}
+            <div>
+              <Label value='username' />
+              <TextInput
+                type='text'
+                placeholder='Username...'
+                id='username'
+                onChange={handlechange}
+              />
+            </div>
+      
+      {/* password */}
+            <div>
+              <Label value='Password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handlechange}
+              />
+            </div>
+      
+      
+      
+      
+          <Button gradientDuoTone='purpleToPink' type='submit' >
+            {/* {
+              loading ? (
+                <>
+              <Spinner size='sm'/>
+              <span className='pl-3'>Loading...</span> 
+              </>
+              )
+              :"Sign In"
+            } */}
+      
+            signin
+          </Button>
+      {/* <Oauth/> */}
+      
+      
+          </form>
+      
+          {/* {
+            error && (
+              <Alert className='mt-5' color='failure'>
+                {error}
+              </Alert>
+            )
+          } */}
+        </div>
+      </div> 
+      
+      
+      
       </div>
     )
   }
